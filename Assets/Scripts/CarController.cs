285585d9554fd12ae3275f964cb0a358fc58c687
kruemelkeksfan <37933687+kruemelkeksfan@users.noreply.github.com>
2021-09-20 15:30:34 +0200

    Switched to 2D Physics.

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -9,35 +9,35 @@ public class CarController : MonoBehaviour
 	[SerializeField] private float brakeForce = 1.0f;
 	[SerializeField] private float steerTorque = 1.0f;
 	private new Transform transform = null;
-	private new Rigidbody rigidbody = null;
+	private new Rigidbody2D rigidbody = null;
 	private Vector2 movement = Vector2.zero;
 
 	private void Start()
 	{
 		transform = gameObject.GetComponent<Transform>();	// Buffer transform, because Component.transform calls GetComponent() under the hood
-		rigidbody = gameObject.GetComponent<Rigidbody>();
+		rigidbody = gameObject.GetComponent<Rigidbody2D>();
 	}
 
-	private void Update()
+	private void FixedUpdate()
 	{
 		// Only process one input per frame, since the state flowchart does not allow for multiple inputs at once
 		// Also intending to brag a bit with how many methods I know to compare floats >.< Hope they are correctly used
 		// ACCELERATE
 		// Dot() to determine if the car is breaking or accelerating backwards
 		// The double check of (movement.y < -float.Epsilon) is not very beautiful, but I have to either double evaluate a condition or write the if-body twice
-		if(movement.y > float.Epsilon || (movement.y < -float.Epsilon && Vector3.Dot(rigidbody.velocity, new Vector3(movement.x, 0.0f, movement.y)) > float.Epsilon))
+		if(movement.y > float.Epsilon || (movement.y < -float.Epsilon && Vector3.Dot(rigidbody.velocity, movement) > float.Epsilon))
 		{
-			rigidbody.AddForce(transform.forward * accelerationForce * movement.y * Time.deltaTime, ForceMode.Impulse);
+			rigidbody.AddForce(transform.up * accelerationForce * movement.y, ForceMode2D.Impulse);
 		}
 		// BRAKE
 		else if(movement.y < -float.Epsilon)
 		{
-			rigidbody.AddForce(transform.forward * brakeForce * movement.y * Time.deltaTime, ForceMode.Impulse);
+			rigidbody.AddForce(transform.up * brakeForce * movement.y, ForceMode2D.Impulse);
 		}
 		// TURN
-		else if(!Mathf.Approximately(movement.x, 0.0f) && rigidbody.velocity != Vector3.zero)								// Unity Vectors use Approximately() implicitly
+		else if(!Mathf.Approximately(movement.x, 0.0f) && rigidbody.velocity != Vector2.zero)								// Unity Vectors use Approximately() implicitly
 		{
-			rigidbody.AddTorque(transform.up * steerTorque * movement.x * Time.deltaTime, ForceMode.Impulse);
+			rigidbody.AddTorque(-steerTorque * movement.x, ForceMode2D.Impulse);
 		}
 	}
 
